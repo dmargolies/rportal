ab9f880b5745446725b19d24a6d4885e6930d9e2
James Martin <james@leftanglemedia.com>
2012-05-08 15:18:09 -0400

    added a couple program features/steps

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -4,3 +4,10 @@ When /^I fill in the form with valid data$/ do
   fill_in "Password", :with => @user.password
 end
 
+Given /^I am signed in as a user$/ do
+  @user = User.create(:email => "foo@bar.com", :password => "password")
+  visit root_url
+  fill_in "Email", :with => @user.email
+  fill_in "Password", :with => @user.password
+  click_button "Sign in"
+end
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -2,3 +2,6 @@ When /^I click "([^"]*)"$/ do |arg1|
   click_button "#{arg1}"
 end
 
+When /^I visit "([^"]*)"$/ do |arg1|
+  visit  arg1
+end
